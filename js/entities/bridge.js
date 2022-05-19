453095af47894af5f1bca9758c17ac0183c1aece
Evan Sklarski <esklarski@gmail.com>
2022-05-19 14:01:47 -0700

    Smooth bridge movement, less likely to strand player. Logic overhaul and collider rects in prep for more changes.

--- js/entities/bridge.js
+++ js/entities/bridge.js
@@ -7,77 +7,56 @@ class Bridge {
         this.offsetY = 0;
         this.width = 35;
         this.height = 31;
-        this.left = this.x;
-        this.right = this.x + 35;
-        this.top = this.y;
-        this.bottom = this.y + 31;
-        this.activeArea = {
-            left: this.x+13,
-            right: this.x + 23,
-            top: this.y,
-            bottom: this.y + 31,
-        }
+
+        this.updateColliders();
         this.held = false;
-       
     }
 
     update() {
-        this.updateCollider();
-        if(rectCollision(this, player.collider)) {
-            //get p
-            if(!this.held){
-                this.offsetX = player.x - this.x;
-                this.offsetY = player.y - this.y;
-            }
+        this.updateColliders();
 
-            if(Key.isDown(Key.SPACE) || gamepad.buttonX()) {
-                this.held = true;
-                if(!rectCollision(player.collider, this.activeArea)) {
+        if(rectCollision(this, player.collider)) {
+            if (rectCollision(this.activeArea, player.collider)) {
+                if(!this.held){
+                    this.offsetX = player.x - this.x;
+                    this.offsetY = player.y - this.y;
+                }
+
+                if(Key.isDown(Key.SPACE) || gamepad.buttonX()) {
+                    this.held = true;
                     this.x = player.x-this.offsetX;
                     this.y = player.y-this.offsetY;
-                    }
-            
-            }else {
-                this.held = false;
-            }
+                } else {
+                    this.held = false;
+                }
 
-            if(rectCollision(this.activeArea, player.collider)) {
                 world.noCollide = true;
                 player.maxSpeed = 20;
                 player.friction = 0.01;
             }
-            if(!rectCollision(this.activeArea, player.collider)) {
+            else {
                 world.noCollide = false;
                 player.friction = 0.85;
                 player.maxSpeed = 400;
             }
         }
-        
-
-        
-
-
-        // console.log(`world.noCollide: ${world.noCollide}`);
     }
     
     draw() {
         canvasContext.drawImage(img["bridge"], Math.floor(this.x-view.x), Math.floor(this.y-view.y) );
-        if(ticker%4 == 0) {
-            fillRect(this.activeArea.left-view.x,
-                this.activeArea.top-view.y,
-                this.activeArea.right-this.activeArea.left,
-                this.activeArea.bottom-this.activeArea.top, '#000000');
-            }
+
         if(rectCollision(this.activeArea, player.collider)) {
             if(ticker%2 == 0 || ticker%3 == 1 || ticker%5 == 0) {
                 fillRect(this.activeArea.left-view.x,
                     this.activeArea.top-view.y,
                     this.activeArea.right-this.activeArea.left,
-                    this.activeArea.bottom-this.activeArea.top, '#8888FF');
-                }
+                    this.activeArea.bottom-this.activeArea.top, '#8888FF'
+                );
+            }
         }
-        //fillRect(this.left-view.x, this.top-view.y, this.right-this.left, this.bottom-this.top, `rgba(255,0,0,0.5)`);
-     }
+
+        // this.debug();
+    }
 
     collect() {
         console.log('collected bridge');
@@ -89,17 +68,43 @@ class Bridge {
         this.y = -100;
     }
 
-    updateCollider() {
+    updateColliders() {
         this.left = this.x-4;
         this.right = this.x + this.width+4;
         this.top = this.y-4;
         this.bottom = this.y + this.height+4;
+
         this.activeArea = {
             left: this.x+13,
             right: this.x + 23,
             top: this.y,
             bottom: this.y + 31,
         }
+
+        this.leftCollider = {
+            left: this.x,
+            right: this.x + 10,
+            top: this.y,
+            bottom: this.y + 30,
+        }
+
+        this.rightCollider = {
+            left: this.x + 25,
+            right: this.x + 34,
+            top: this.y,
+            bottom: this.y + 30,
+        }
     }
 
+    debug() {
+        // object rect
+        // fillRect(this.left-view.x, this.top-view.y, this.right-this.left, this.bottom-this.top, `rgba(255,0,0,0.5)`);
+
+        // active area rect
+        fillRectangle(this.activeArea, `rgba(0,0,255,0.3)`);
+
+        // collider rects
+        fillRectangle(this.leftCollider, `rgba(0,255,0,0.3)`);
+        fillRectangle(this.rightCollider, `rgba(0,255,0,0.3)`);
+    }
 }
